7d7d03689d297b4320d51488f7c847755b4b1a08
springlol <353256142@qq.com>
2016-11-06 23:17:44 +0800

    测试

--- src/main/java/com/zt/weixin/entity/User.java
+++ src/main/java/com/zt/weixin/entity/User.java
@@ -9,6 +9,15 @@ public class User {
     public String id;
     public String name;
     public String password;
+    public String code;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 
     public String getId() {
         return id;
